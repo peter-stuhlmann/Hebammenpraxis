214272e119d0c6cf773e63a6ed20852fe04a1dc8
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2022-01-19 21:22:56 +0100

    Scroll to top on route change

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,11 +1,17 @@
-import React, { lazy } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { useEffect, lazy } from 'react';
+import { Switch, Route, useLocation } from 'react-router-dom';
 
 const Home = lazy(() => import('../pages/Home'));
 const Contact = lazy(() => import('../pages/Contact'));
 const NotFound = lazy(() => import('../pages/NotFound'));
 
 export default function Router() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <Switch>
       <Route exact path="/" component={Home} />
