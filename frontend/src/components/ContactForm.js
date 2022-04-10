840c6cfb72877d79fc274a482fa534ed0fb9a5db
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2022-04-10 19:02:07 +0200

    Add phone number to contact form post request

--- frontend/src/components/ContactForm.js
+++ frontend/src/components/ContactForm.js
@@ -37,6 +37,7 @@ export default function ContactForm(props) {
     name: '',
     address: '',
     email: '',
+    phone: '',
     delivery: '',
     gravida: '',
     para: '',
--- server/routes/send-message.js
+++ server/routes/send-message.js
@@ -23,12 +23,13 @@ router.post('/', (req, res) => {
   const name = req.body.name;
   const address = req.body.address;
   const email = req.body.email;
+  const phone = req.body.phone;
   const delivery = req.body.delivery;
   const gravida = req.body.gravida;
   const para = req.body.para;
   const message = req.body.message;
-  const html = `<b>Name:</b> ${name}<br /><b>Adresse:</b> ${address}<br /><b>E-Mail:</b> ${email}<br /><b>ET:</b> ${delivery}<br /><b>Gravida:</b> ${gravida}<br /><b>Para:</b> ${para}<br /><b>Nachricht:</b> ${message}<br /><br /><i>Diese Anfrage wurde über das Betreuungsformular auf der Website gestellt.</i>`;
-  const text = `Name: ${name} \nAdresse: ${address} \nE-Mail: ${email} \nET: ${delivery} \nGravida: ${gravida} \nPara: ${para} \nNachricht: ${message} \n\n Diese Anfrage wurde über das Betreuungsformular auf der Website gestellt.`;
+  const html = `<b>Name:</b> ${name}<br /><b>Adresse:</b> ${address}<br /><b>E-Mail:</b> ${email}<br /><b>Telefon:</b> ${phone}<br /><b>ET:</b> ${delivery}<br /><b>Gravida:</b> ${gravida}<br /><b>Para:</b> ${para}<br /><b>Nachricht:</b> ${message}<br /><br /><i>Diese Anfrage wurde über das Betreuungsformular auf der Website gestellt.</i>`;
+  const text = `Name: ${name} \nAdresse: ${address} \nE-Mail: ${email} \nTelefon: ${phone} \nET: ${delivery} \nGravida: ${gravida} \nPara: ${para} \nNachricht: ${message} \n\n Diese Anfrage wurde über das Betreuungsformular auf der Website gestellt.`;
 
   const mail = {
     from: {
